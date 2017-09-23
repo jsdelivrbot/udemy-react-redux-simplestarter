4eba91805c8f63e14087e48fbd403580aa432864
Merilee Wheelock <eelirem@gmail.com>
2017-09-22 19:41:45 -0700

    added lodash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import API_KEY from './config';
 import SearchBar from './components/search_bar';
@@ -18,7 +19,7 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		this.videoSearch('surfboards');
+		this.videoSearch('avett brothers');
 	}
 
 	videoSearch(term){
@@ -33,9 +34,11 @@ class App extends Component {
 	}
 
 	render(){
+		const videoSearch = _.debounce((term)=>{ this.videoSearch(term) }, 300);
+
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList
 					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
