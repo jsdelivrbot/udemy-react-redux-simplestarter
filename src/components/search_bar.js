13989d7b319ede5cd6c0535df714702be10f0c55
Merilee Wheelock <eelirem@gmail.com>
2017-09-22 17:04:11 -0700

    finish section 1

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+	constructor(props){
+		// This is how we initialize state in a class based component
+		super(props);
+
+		this.state = { term: 'Starting value' };
+	}
+
 	render(){
-		return <input onChange={event => console.log(event.target.value)} />;
+		return(
+			<div>
+				<input
+					value = {this.state.term}
+					onChange={event => this.setState({ term: event.target.value })}
+				/>
+				{/*Value of the input: {this.state.term}*/}
+			</div>
+		);
 	}
 }
 
